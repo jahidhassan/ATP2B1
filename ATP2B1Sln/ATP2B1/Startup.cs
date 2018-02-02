0dbc01e246093a0216d72c13968d82396171e0af
Jahid Hassan <Jahid Hassan@DESKTOP-DMC0B0D>
2018-02-02 12:50:25 +0600

    Initialize

---
+++ ATP2B1Sln/ATP2B1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ATP2B1.Startup))]
+namespace ATP2B1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
